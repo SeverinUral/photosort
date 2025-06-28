4a1929a8011a92acd42165f70258eed0c4ee74d3
Alex Fomenko <alexfomg@gmail.com>
2025-06-28 17:30:47 +0500

    Add auto build fot ded package

---
+++ build_deb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Fomenko A V  2025 (c)
+
+cp -vf photosort deb/photo_sort/usr/bin/photosort
+
+chmod +x deb/photo_sort/usr/bin/photosort
+
+echo "Enter Version (x.x-x.x):"
+read VERSION
+
+rm -rfv deb/parentalcontrol/DEBIAN/control 
+
+CONTROL_FILE="Package: photosort
+Version: $VERSION
+Section: graphics
+Architecture: all
+Priority: optional
+Depends: python3, python3-pip, python3-exifread
+Maintainer: Alex <alexfomg@gmail.com>
+Description: Programm for sorting shots by creation date
+Installed-Size: $(du -sb deb/photo_sort | grep -o '^[0-9]*')" 
+
+echo "$CONTROL_FILE" > deb/photo_sort/DEBIAN/control 
+
+cd deb/
+./make_deb.sh 
